e4a355ae7448003f24197e33d13c293d6f47303f
soto <kiso.soto@gmail.com>
2025-03-22 19:52:24 +0900

    view,css,コントローラの追加

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECH</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    @yield('css')
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a href="" class="header__logo">COACHTECH</a>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
